f468e2f6a086b8d79a2480b721631e518873ec79
Umakant Patil <umakantpatil2712@gmail.com>
2025-09-24 17:10:37 +0530

    config change

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,10 @@ const nextConfig = {
   images: {
     domains: ["happenings.lpu.in"], // <-- ADD THIS
   },
+  experimental: {
+    appDir: true, // ensure App Router is enabled
+  },
+  output: "standalone",
 };
 
 export default nextConfig;
