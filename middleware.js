4f6a4840b63e2ffdc3b981a61e15fbc046649608
Umakant Patil <umakantpatil2712@gmail.com>
2025-11-25 23:14:03 +0530

    middleware changed

--- middleware.js
+++ middleware.js
@@ -1,35 +1,26 @@
-import Cookies from "js-cookie";
 import { NextResponse } from "next/server";
 
 export function middleware(request) {
-  const token =
-    request.cookies.get("token")?.value ||
-    request.headers.get("authorization")?.split("Bearer ")[1] ||
-    null;
-
-  //   const token = request.cookies.get("token")?.value || null;
+  const token = request.cookies.get("token")?.value;
   const { pathname } = request.nextUrl;
 
-  console.log("path name:", pathname);
-  // Allow access to root and static/public files (like favicon, images, etc.)
+  console.log("Middleware token:", token);
+
   const isPublicPath =
     pathname === "/" ||
     pathname === "/register" ||
+    pathname.startsWith("/login") ||
     pathname.startsWith("/_next") ||
     pathname.startsWith("/images") ||
-    pathname.startsWith("/favicon.ico") ||
-    pathname.startsWith("/login");
+    pathname.startsWith("/favicon");
 
-  // Allow if public or if token exists
-  console.log("isPublicPath:", isPublicPath, "Token:", token);
   if (isPublicPath || token) {
     return NextResponse.next();
   }
 
-  //  If not logged in and trying to access any private route
   return NextResponse.redirect(new URL("/?showLogin=true", request.url));
 }
 
 export const config = {
-  matcher: ["/((?!api|_next|static|.*\\..*).*)"], // matches everything except static files
+  matcher: ["/((?!api|_next|static|.*\\..*).*)"],
 };
